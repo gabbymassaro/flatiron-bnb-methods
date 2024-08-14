2bea7e023a9b8074b205ff8c93ea29a9d60f7f2d
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-14 12:29:45 -0400

    Use sql to find city_openings

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,18 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(start_date, end_date)
+    Listing.find_by_sql([
+      "SELECT listings.*
+      FROM listings
+      LEFT JOIN reservations
+      ON listings.id = reservations.listing_id
+      AND NOT (
+          reservations.checkin > ?
+          OR reservations.checkout < ?
+      )
+      WHERE reservations.id IS NULL",
+      end_date, start_date
+    ])
+  end
 end
-
