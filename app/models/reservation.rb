b761c68a08a245072b554d2dcf81f7b3e8c1c8dd
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-14 22:35:02 -0400

    Fix some reservation validations without breaking other tests

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,13 +3,12 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
-  validates :checkin, :checkout, presence: true
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+
 
   validate :no_host_reservations
-  validate :listing_available_at_checkin
-  validate :listing_available_at_checkout
-  validate :checkin_before_checkout
-  validate :checkin_and_checkout_not_same
+  validate :invalid_checkins
 
 
   def duration
@@ -21,47 +20,19 @@ class Reservation < ActiveRecord::Base
   end
 
   private
+
   def no_host_reservations
     if guest_id == listing.host_id
       errors.add(:guest, "cannot be the same as the host")
     end
   end
 
-  def listing_available_at_checkin
-    if listing.reservations.where("checkin <= ? AND checkout >= ?", checkin, checkin).exists?
-      errors.add(:checkin, "is not available for the selected listing")
+  def invalid_checkins
+    if checkout && checkin && checkout <= checkin
+      errors.add(:guest_id, "Invalid reservations dates")
     end
   end
 
-  def listing_available_at_checkout
-    if listing.reservations.where("checkin <= ? AND checkout >= ?", checkout, checkout).exists?
-      errors.add(:checkout, "is not available for the selected listing")
-    end
+  def unavailable_listings
   end
-
-  def checkin_before_checkout
-    return if checkin.nil? || checkout.nil?
-
-    if checkin >= checkout
-      errors.add(:checkout, "must be after checkin date")
-    end
-  end
-
-  def checkin_and_checkout_not_same
-    return if checkin.nil? || checkout.nil?
-
-    if checkin == checkout
-      errors.add(:checkout, "cannot be the same as checkin date")
-    end
-  end
-  # ---- this will break other tests ----- #
-  # def invalid_reservation_dates
-  #   bad_dates = Reservation.where(listing_id: listing_id)
-  #                          .where.not(id: id)
-  #                          .where("checkin < ? OR checkout > ? ",
-  #                          checkout, checkin)
-  #   if bad_dates.exists?
-  #     errors.add(:checkin, "cannot reserve")
-  #   end
-  # end
 end
